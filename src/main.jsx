7c460f0413bd2ae4fc10493bcf645b988bde746b
Dhruv Patel <dhulopatel27@gmail.com>
2024-08-25 21:43:15 +0530

    connect cleark with supabase.

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ClerkProvider } from "@clerk/clerk-react";
 import { shadesOfPurple } from "@clerk/themes";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 // Import your publishable key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
